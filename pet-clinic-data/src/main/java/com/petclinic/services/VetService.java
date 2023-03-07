d5e39658b058e4523347fa4c8d734d2488f32dcb
velkogenchev <velkogenchev@gmail.com>
2023-03-07 12:23:47 +0200

    Adding interfaces for services

---
+++ pet-clinic-data/src/main/java/com/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package com.petclinic.services;
+
+import com.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
