cc3e4dec7b5b290a3685cb0e2806f73aa0a10949
velkogenchev <velkogenchev@gmail.com>
2023-03-07 12:34:29 +0200

    Adding crud interface

--- pet-clinic-data/src/main/java/com/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/petclinic/services/PetService.java
@@ -2,10 +2,5 @@ package com.petclinic.services;
 
 import com.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
