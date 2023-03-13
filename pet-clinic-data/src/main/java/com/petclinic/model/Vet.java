c2a20d782a019f0ab6556dd51fd35ccd63e62a9b
velkogenchev <velkogenchev@gmail.com>
2023-03-13 16:02:48 +0200

    adding entity annotations

--- pet-clinic-data/src/main/java/com/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/petclinic/model/Vet.java
@@ -1,9 +1,19 @@
 package com.petclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id")
+    )
     private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialties() {
